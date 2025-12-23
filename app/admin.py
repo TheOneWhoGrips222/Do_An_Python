3e035911c01eb1595abc9e93c7a57842146816c0
Bao <23130024@st.hcmuaf.edu.vn>
2025-12-24 00:19:37 +0700

    update

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from .models import Question, Answer, Tags, TagItem, Report
+from .models import Question, Answer, Tag, TagItem, Report, Users
 
-
-
+admin.site.register(Users)
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Tags)
+admin.site.register(Tag)
 admin.site.register(TagItem)
 admin.site.register(Report)
 
--- app/models.py
+++ app/models.py
@@ -2,12 +2,22 @@
 from django.db import models
 from django.conf import settings
 
+#---Bangr User
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=130)
+    createDate = models.DateTimeField(auto_now_add=True)
+    reputation = models.IntegerField(default=0)
+    llike = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
 
 
 
 # --- Bảng Question (Câu hỏi) ---
 class Question(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     Title = models.CharField(max_length=200)
     Body = models.TextField()
     CreationDate = models.DateTimeField(auto_now_add=True)
@@ -25,8 +35,8 @@ class Question(models.Model):
 
 # --- Bảng Answer (Câu trả lời) ---
 class Answer(models.Model):
-    id = models.IntegerField(primary_key=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')  # Liên kết đến Question
+    id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     AnswerDate = models.DateTimeField(auto_now_add=True)
     AnswerScore = models.IntegerField(default=0)
     AnswerBody = models.TextField()
@@ -39,19 +49,24 @@ class Answer(models.Model):
         ordering = ['-AnswerScore', 'AnswerDate']
 
 
+
 # --- Bảng Tags ---
-class Tags(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+class Tag(models.Model):
+    id = models.CharField(primary_key=True, max_length=50, unique=True);
+    createDate = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.id
 
 class TagItem(models.Model):
     q_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='questions')
-    tag_id = models.ForeignKey(Tags, on_delete=models.CASCADE, related_name='tags')
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tag')
     class Meta:
         unique_together = (('q_id', 'tag_id'),)
 
     def __str__(self):
-        return self.name
+        return f"Tag for {self.q_id.Title}"
+
 class Report(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     body = models.TextField()
     q_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='reports')
